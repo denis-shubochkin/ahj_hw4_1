cc3cf1b9d6546e495b6ed671139c0d3931785ceb
Denis.Shubochkin <zippo1095@gmail.com>
2020-09-22 23:30:23 +0300

    hw4_1 first ver

---
+++ src/js/_tests_/app.test.js
@@ -0,0 +1,41 @@
+import {checkPaySystem} from '../app'
+import {checkNumber} from '../app'
+import {luhnAlgorithm} from '../app'
+
+test.each([
+    ['Visa check','40131111','VISA'],
+    ['MasterCard check','50161111','MasterCard'],
+    ['AE check','50121111','American Express'],
+    ['Diners check','50191111','Diners'],
+    ['Discover check','40221111','Discover'],
+    ['JCB check','40211111','JCB'],
+    ['МИР check','40181111','МИР'],
+    ['Не определено','1111111','Платежная система не определена']
+]) (('%s'),(_,input,expected) => {
+    expected(checkPaySystem(input)).toBe(expected);
+});
+
+test('short number', () => {
+    const num='1234567';
+    expect(checkNumber(num)).toBe('Incorrect length');
+});
+
+test('long number', () => {
+    const num='123456723123123123123123123';
+    expect(checkNumber(num)).toBe('Incorrect length');
+});
+
+test('correct number', () => {
+    const num='1234 4453 5554 7777';
+    expect(checkNumber(num)).toBe(1);
+});
+
+test('incorrect luhn', () => {
+    const num='1234 1234 1234 1234';
+    expect(checkNumber(num)).toBe('Incorrect luhn');
+});
+
+test('correct luhn', () => {
+    const num='1234 1234 1234 1238';
+    expect(checkNumber(num)).toBe(1);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -4,27 +4,100 @@
 const butn = document.querySelector('.check-button');
 const input = document.querySelector('.input');
 const incorrectLabel = document.querySelector('.hidden-incorrect');
+const luhnLabel = document.querySelector('.hidden-luhn');
 const correctLabel = document.querySelector('.hidden-correct');
+const labels = document.querySelector('.labels');
 butn.addEventListener('click', checkCorrect);
+input.addEventListener('keyup',showPaySystem);
 let content;
 
-function checkCorrect(event) {
-  event.preventDefault();
-  content = input.value.replace(/\s/g, '');
-  console.log(luhnAlgorithm(content));
-  if(content.length!==16)
-   {
-    incorrectLabel.style.display = 'block';
-   }
-   else
-   {
-    incorrectLabel.style.display = 'none';
-    correctLabel.style.display = 'block';
+ function clearLabelsErrors() {
+  incorrectLabel.style.display = 'none';
+  luhnLabel.style.display = 'none'; 
+  correctLabel.style.display = 'none';
+}
+
+   function checkPaySystem(cont)
+{
+  cont = cont.replace(/\s/g, '');
+  if(cont.startsWith('4013'))
+  {
+    return 'VISA';
+  }
+  else if(cont.startsWith('5016'))
+  {
+    return 'MasterCard';
+  }
+  else if(cont.startsWith('5012'))
+  {
+    return 'American Express';
+  }
+  else if(cont.startsWith('5019'))
+  {
+    return 'Diners';
+  }
+  else if(cont.startsWith('4022'))
+  {
+    return 'Discover';
+  }
+  else if(cont.startsWith('4021'))
+  {
+    return 'JCB';
+  }
+  else if(cont.startsWith('4018'))
+  {
+    return 'МИР';
+  }
+  else 
+  {
+    return 'Платежная система не определена';
+  }
+}
+
+  function showPaySystem() {
+  let card = document.querySelector('.card');
+  if(card) {card.remove();}
+  content = input.value;
+  labels.insertAdjacentHTML('beforebegin',`<div class="card">${checkPaySystem(content)}</div>`)
+}
+
+  function checkNumber(cont) {
+  cont = cont.replace(/\s/g, '');
+  if(cont.length!==16)
+  {
+    return 'Incorrect length';
+  }
+  else if (!luhnAlgorithm(cont))
+  {
+    return 'Incorrect luhn';
+  }
+  else
+  { 
+    return 1;
    }
 }
 
+  function checkCorrect(event) {
+  event.preventDefault();
+  clearLabelsErrors();
+  content = input.value;
+  if(checkNumber(content)==='Incorrect length')
+  {
+    incorrectLabel.style.display = 'block';
+  }
+  else if(checkNumber(content)==='Incorrect luhn')
+  {
+    luhnLabel.style.display = 'block';  
+  }
+  else if(checkNumber(content)===1)
+  {
+    correctLabel.style.display = 'block';  
+  }
+  
+}
 
-function luhnAlgorithm(digits) {
+
+  function luhnAlgorithm(digits) {
   let sum = 0;
 
   for (let i = 0; i < digits.length; i++) {
